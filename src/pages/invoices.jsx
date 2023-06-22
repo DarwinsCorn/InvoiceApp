d830a97e6b6364694a7cc4b8d43d4b61b9ec7e07
DarwinsCorn <dietrevich@gmail.com>
2023-06-22 12:32:39 -0400

    updated the look of the invoice page

--- src/pages/invoices.jsx
+++ src/pages/invoices.jsx
@@ -6,6 +6,9 @@ import {data as vendData} from '../data/vendorData';
 
 export default function Invoices() {
 
+    const date = new Intl.DateTimeFormat('en-us',{ dateStyle: "long"});
+    const currency = new Intl.NumberFormat('en-us',{ style:'currency',currency:'USD'})
+
     function findVend(id) {
         return vendData.filter( vend => vend.id === id)[0];
     }
@@ -27,14 +30,14 @@ export default function Invoices() {
             <div className={classes.strip}>
                 <div>
                     <h2 className={classes.invcolor}>Invoice #{inv.id}</h2>
-                    <h2>{inv.date}</h2>
+                    <h2 className={classes.normalFont}>{date.format(new Date(inv.date))}</h2>
                 </div>
                 <div>
                     <h2>{findVend(inv.vendorId).name}</h2>
-                    <h2>{findVend(inv.vendorId).email}</h2>
+                    <h2 className={classes.normalFont}>{findVend(inv.vendorId).email}</h2>
                 </div>
-                <div>
-                    <h2>USD ${inv.amt}</h2>
+                <div className={classes.rightAlign}>
+                    <h2>{currency.format(inv.amt)}</h2>
                 </div>
             </div>
         </Link>
@@ -42,7 +45,7 @@ export default function Invoices() {
 
     return(
         <div className={classes.center}>
-            <div className={classes.block}>
+            <div className={classes.blockMenu}>
                 <div className={classes.cardMenu}>
                     <h1>Vendors</h1>
                     <h1>{numVend()}</h1>
@@ -50,7 +53,7 @@ export default function Invoices() {
                 <div className={classes.cardMenu}>
                     <h1>Invoices</h1>
                     <h1>{invData.length}</h1>
-                    <h1>Total: USD ${allTotalInv()}</h1>
+                    <h1>{currency.format(allTotalInv())}</h1>
                 </div>
             </div>
             <div className={classes.cardInvoices}>
