1c9964b15fb0c46003d9da35ee0ff8ca746b5704
DarwinsCorn <dietrevich@gmail.com>
2023-07-02 09:41:52 -0400

    clean up useless code in addVendorForm

--- src/components/addVendorForm.jsx
+++ src/components/addVendorForm.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import classes from '../css/addVendor.module.css';
 import classes from '../css/addVendorForm.module.css';
 
 export default function AddVendorForm({set,add}) {
@@ -16,7 +15,7 @@ export default function AddVendorForm({set,add}) {
         <div className={classes.container}>
             <form onSubmit={formHandler}>
                 <div className={classes.center}>
-                    <input required className={classes.inputForm} placeholder="Vendor Name" type="text" name="name" id="name" />
+                    <input required className={classes.inputForm} placeholder="Vendor Name" type="text" name="name" id="name"  />
                     <input required className={classes.inputForm} placeholder="City" type="text" name="city" id="city" />
                     <input required className={classes.inputForm} placeholder="Street" type="text" name="street" id="street" />
                     <input required className={classes.inputForm} placeholder="State" type="text" name="state" id="state" />
