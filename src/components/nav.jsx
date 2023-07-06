f2dee50fc22fa6932af1a685a6e0dc0c2fb1adc9
DarwinsCorn <dietrevich@gmail.com>
2023-07-06 10:18:57 -0400

    function to address the reseting of demo data and functionality

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import '../css/nav.css';
 import { Link } from "react-router-dom";
+import '../css/nav.css';
 
 
 export default function Nav() {
+
+    function reset() {
+        sessionStorage.clear();
+        location.reload(true)
+    }
 
     return(
         <nav>
             <Link to={"/"}>
             <h1 className="logo">#LOGO</h1>
             </Link>
-            <h2 className="user">USER</h2>
+            <h2 onClick={reset} className="user cursor">RESET DEMO</h2>
         </nav>
     )
 }
